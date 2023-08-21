06576965ece35467bf3a1b0cf96a2ddb1bcb2a02
Maja Kabus <mkabus@cern.ch>
2023-08-21 15:01:40 +0200

    Added logs, with ^M

--- Tutorials/src/test.cxx
+++ Tutorials/src/test.cxx
@@ -24,153 +24,163 @@ using namespace o2::soa;
 using namespace o2::framework;
 using namespace o2::framework::expressions;
 
-
 struct DeltaEtaHistograms {
-  OutputObj<TH1F> deltaEtaTwoForLoops{TH1F("deltaEtaTwoForLoops", "#Delta#eta", 40,-2,2)};
-  OutputObj<TH1F> deltaEtaFullNoIndexCheck{TH1F("deltaEtaFullNoIndexCheck", "#Delta#eta", 40,-2,2)};
-  OutputObj<TH1F> deltaEtaFull{TH1F("deltaEtaFull", "#Delta#eta", 40,-2,2)};
-  OutputObj<TH1F> deltaEtaUpper{TH1F("deltaEtaUpper", "#Delta#eta", 40,-2,2)};
-  OutputObj<TH1F> deltaEtaStrictlyUpper{TH1F("deltaEtaStrictlyUpper", "#Delta#eta", 40,-2,2)};
+  OutputObj<TH1F> deltaEtaTwoForLoops{TH1F("deltaEtaTwoForLoops", "#Delta#eta", 40, -2, 2)};
+  OutputObj<TH1F> deltaEtaFullNoIndexCheck{TH1F("deltaEtaFullNoIndexCheck", "#Delta#eta", 40, -2, 2)};
+  OutputObj<TH1F> deltaEtaFull{TH1F("deltaEtaFull", "#Delta#eta", 40, -2, 2)};
+  OutputObj<TH1F> deltaEtaUpper{TH1F("deltaEtaUpper", "#Delta#eta", 40, -2, 2)};
+  OutputObj<TH1F> deltaEtaStrictlyUpper{TH1F("deltaEtaStrictlyUpper", "#Delta#eta", 40, -2, 2)};
+
+  bool processedLoops = false;
+  bool processedStrictlyUpper = false;
 
   void processTwoForLoops(aod::Collisions const& collisions, aod::FullTracks& tracks)
   {
+    if (processedLoops) {
+      return;
+    }
+
     int counter = 0;
-    for (auto& c : collisions) 
-    {
-      if(counter >= 10)
-        continue;
-    
-      // create the partition groupedTracks
-      // create the partition groupedTracks
+    for (auto& c : collisions) {
+      if (counter >= 1) {
+        break;
+      }
+
       Partition<aod::FullTracks> groupedTracks1 = (aod::track::collisionId == c.globalIndex()) && (aod::track::pt < 0.5f);
       Partition<aod::FullTracks> groupedTracks2 = (aod::track::collisionId == c.globalIndex()) && (aod::track::pt >= 0.5f && aod::track::pt < 1.0f);
       groupedTracks1.bindTable(tracks);
       groupedTracks2.bindTable(tracks);
 
-      for (auto& track1 : groupedTracks1) 
-      {
-        for (auto& track2 : groupedTracks2) 
-        {
-          if(track1.index() >= track2.index())
-            continue;
+      LOG(info) << "Two loops collision: " << c.globalIndex() << " tracks: " << groupedTracks1.size() << ", " << groupedTracks2.size();
 
-          float deltaEta = track1.eta()-track2.eta();
-          deltaEtaTwoForLoops->Fill(deltaEta);
+      for (auto& track1 : groupedTracks1) {
+        for (auto& track2 : groupedTracks2) {
+          if (track1.index() < track2.index()) {
+            float deltaEta = track1.eta() - track2.eta();
+            LOG(info) << "Two loops filling for tracks: " << track1.globalIndex() << ", " << track2.globalIndex() << " ind: " << track1.index() << ", " << track2.index() << " eta: " << track1.eta() << ", " << track2.eta() << " delta: " << deltaEta;
+            deltaEtaTwoForLoops->Fill(deltaEta);
+          }
         }
       }
 
       counter++;
     }
+    processedLoops = true;
   }
   PROCESS_SWITCH(DeltaEtaHistograms, processTwoForLoops, "Enable two for loops", true);
 
   void processFullIndexPolicyNoIndexCheck(aod::Collisions const& collisions, aod::FullTracks& tracks)
   {
     int counter = 0;
-    for (auto& c : collisions) 
-    {
-      if(counter >= 10)
-        continue;
-    
+    for (auto& c : collisions) {
+      if (counter >= 1) {
+        break;
+      }
+
       // create the partition groupedTracks
       Partition<aod::FullTracks> groupedTracks1 = (aod::track::collisionId == c.globalIndex()) && (aod::track::pt < 0.5f);
       Partition<aod::FullTracks> groupedTracks2 = (aod::track::collisionId == c.globalIndex()) && (aod::track::pt >= 0.5f && aod::track::pt < 1.0f);
       groupedTracks1.bindTable(tracks);
       groupedTracks2.bindTable(tracks);
 
-      for (auto& [track1, track2] : combinations(CombinationsFullIndexPolicy(groupedTracks2, groupedTracks2)))
-      {
-          float deltaEta = track1.eta()-track2.eta();
-          deltaEtaFullNoIndexCheck->Fill(deltaEta);
+      for (auto& [track1, track2] : combinations(CombinationsFullIndexPolicy(groupedTracks2, groupedTracks2))) {
+        float deltaEta = track1.eta() - track2.eta();
+        deltaEtaFullNoIndexCheck->Fill(deltaEta);
       }
 
       counter++;
     }
   }
-  PROCESS_SWITCH(DeltaEtaHistograms, processFullIndexPolicyNoIndexCheck, "Enable full index no index Check", true);
+  PROCESS_SWITCH(DeltaEtaHistograms, processFullIndexPolicyNoIndexCheck, "Enable full index no index Check", false);
 
   void processFullIndexPolicy(aod::Collisions const& collisions, aod::FullTracks& tracks)
   {
     int counter = 0;
-    for (auto& c : collisions) 
-    {
-      if(counter >= 10)
-        continue;
-    
+    for (auto& c : collisions) {
+      if (counter >= 1) {
+        break;
+      }
+
       // create the partition groupedTracks
       Partition<aod::FullTracks> groupedTracks1 = (aod::track::collisionId == c.globalIndex()) && (aod::track::pt < 0.5f);
       Partition<aod::FullTracks> groupedTracks2 = (aod::track::collisionId == c.globalIndex()) && (aod::track::pt >= 0.5f && aod::track::pt < 1.0f);
       groupedTracks1.bindTable(tracks);
       groupedTracks2.bindTable(tracks);
 
-      for (auto& [track1, track2] : combinations(CombinationsFullIndexPolicy(groupedTracks2, groupedTracks2)))
-      {
-          if(track1.index() >= track2.index())
-            continue;
+      LOG(info) << "Full policy collision: " << c.globalIndex() << " tracks: " << groupedTracks1.size() << ", " << groupedTracks2.size();
 
-          float deltaEta = track1.eta()-track2.eta();
+      for (auto& [track1, track2] : combinations(CombinationsFullIndexPolicy(groupedTracks2, groupedTracks2))) {
+        if (track1.index() < track2.index()) {
+          float deltaEta = track1.eta() - track2.eta();
+          LOG(info) << "Full policy filling for tracks: " << track1.globalIndex() << ", " << track2.globalIndex() << " ind: " << track1.index() << ", " << track2.index() << " eta: " << track1.eta() << ", " << track2.eta() << " delta: " << deltaEta;
           deltaEtaFull->Fill(deltaEta);
+        }
       }
 
       counter++;
     }
   }
-  PROCESS_SWITCH(DeltaEtaHistograms, processFullIndexPolicy, "Enable full index", true);
+  PROCESS_SWITCH(DeltaEtaHistograms, processFullIndexPolicy, "Enable full index", false);
 
   void processUpperIndexPolicy(aod::Collisions const& collisions, aod::FullTracks& tracks)
   {
     int counter = 0;
-    for (auto& c : collisions) 
-    {
-      if(counter >= 10)
-        continue;
-    
+    for (auto& c : collisions) {
+      if (counter >= 1) {
+        break;
+      }
+
       // create the partition groupedTracks
       Partition<aod::FullTracks> groupedTracks1 = (aod::track::collisionId == c.globalIndex()) && (aod::track::pt < 0.5f);
       Partition<aod::FullTracks> groupedTracks2 = (aod::track::collisionId == c.globalIndex()) && (aod::track::pt >= 0.5f && aod::track::pt < 1.0f);
       groupedTracks1.bindTable(tracks);
       groupedTracks2.bindTable(tracks);
 
-      for (auto& [track1, track2] : combinations(CombinationsUpperIndexPolicy(groupedTracks2, groupedTracks2)))
-      {
-          if(track1.index() >= track2.index())
-            continue;
+      LOG(info) << "Upper policy collision: " << c.globalIndex() << " tracks: " << groupedTracks1.size() << ", " << groupedTracks2.size();
 
-          float deltaEta = track1.eta()-track2.eta();
+      for (auto& [track1, track2] : combinations(CombinationsUpperIndexPolicy(groupedTracks2, groupedTracks2))) {
+        if (track1.index() < track2.index()) {
+          float deltaEta = track1.eta() - track2.eta();
+          LOG(info) << "Upper policy filling for tracks: " << track1.globalIndex() << ", " << track2.globalIndex() << " ind: " << track1.index() << ", " << track2.index() << " eta: " << track1.eta() << ", " << track2.eta() << " delta: " << deltaEta;
           deltaEtaUpper->Fill(deltaEta);
+        }
       }
 
       counter++;
     }
   }
-  PROCESS_SWITCH(DeltaEtaHistograms, processUpperIndexPolicy, "Enable upper index", true);
+  PROCESS_SWITCH(DeltaEtaHistograms, processUpperIndexPolicy, "Enable upper index", false);
 
   void processStrictlyUpper(aod::Collisions const& collisions, aod::FullTracks& tracks)
   {
+    if (processedStrictlyUpper) {
+      return;
+    }
     int counter = 0;
-    for (auto& c : collisions) 
-    {
-      if(counter >= 10)
-        continue;
-    
+    for (auto& c : collisions) {
+      if (counter >= 1) {
+        break;
+      }
+
       // create the partition groupedTracks
       Partition<aod::FullTracks> groupedTracks1 = (aod::track::collisionId == c.globalIndex()) && (aod::track::pt < 0.5f);
       Partition<aod::FullTracks> groupedTracks2 = (aod::track::collisionId == c.globalIndex()) && (aod::track::pt >= 0.5f && aod::track::pt < 1.0f);
       groupedTracks1.bindTable(tracks);
       groupedTracks2.bindTable(tracks);
 
-      for (auto& [track1, track2] : combinations(CombinationsStrictlyUpperIndexPolicy(groupedTracks2, groupedTracks2)))
-      {
-          float deltaEta = track1.eta()-track2.eta();
-          deltaEtaStrictlyUpper->Fill(deltaEta);
+      LOG(info) << "Strictly upper policy collision: " << c.globalIndex() << " tracks: " << groupedTracks1.size() << ", " << groupedTracks2.size();
+
+      for (auto& [track1, track2] : combinations(CombinationsStrictlyUpperIndexPolicy(groupedTracks2, groupedTracks2))) {
+        float deltaEta = track1.eta() - track2.eta();
+        LOG(info) << "Strictly upper policy filling for tracks: " << track1.globalIndex() << ", " << track2.globalIndex() << " ind: " << track1.index() << ", " << track2.index() << " eta: " << track1.eta() << ", " << track2.eta() << " delta: " << deltaEta;
+        deltaEtaStrictlyUpper->Fill(deltaEta);
       }
 
       counter++;
     }
+    processedStrictlyUpper = true;
   }
   PROCESS_SWITCH(DeltaEtaHistograms, processStrictlyUpper, "Enable strictly upper", true);
-
-
 };
 
 WorkflowSpec defineDataProcessing(ConfigContext const& cfgc)
