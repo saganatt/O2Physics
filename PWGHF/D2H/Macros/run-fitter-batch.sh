fa6b688d137feb6ce0f5da1f0a397afad4354f5b
saganatt <8majak8@gmail.com>
2024-06-18 18:27:48 +0200

    Fix config and run script for successful batch run

--- PWGHF/D2H/Macros/run-fitter-batch.sh
+++ PWGHF/D2H/Macros/run-fitter-batch.sh
@@ -11,25 +11,20 @@ for dir in ${INPUT_PATTERN}* ; do
 
   suffix=${dir##${INPUT_PATTERN}}
   echo $suffix
-
-  RESDIR="/data8/majak/d2h-fitter/100624/results-probs-bkg_${suffix}"
-  RESPATH="${RESDIR}/fits.root"
-  
-  mkdir "${RESDIR}"
   
   ROOT_EXT=".root"
   suffix_no_ext=${suffix%%${ROOT_EXT}}
   echo $suffix_no_ext
+
+  RESDIR="/data8/majak/d2h-fitter/100624/results-probs-bkg_${suffix_no_ext}"
+  mkdir "${RESDIR}"
 
   CUR_CFG="${CONFIG}_${suffix_no_ext}_edit.json"
   cp "${CONFIG_EXT}" "${CUR_CFG}" 
   sed -i "s/%infile%/projections_${suffix}/g" "${CUR_CFG}" || exit 1
 
   sed -i "s/%outdir%/results-probs-bkg_${suffix_no_ext}/g" "${CUR_CFG}" || exit 1
+  sed -i "s/%sf%/${suffix_no_ext}/g" "${CUR_CFG}" || exit 1
 
-  root -b -l -q <<EOF
-    .x HFInvMassFitter.cxx ;
-    .L runMassFitter.C;
-    runMassFitter(\"${CUR_CFG}\");
-EOF
+  root -b -l -q -x "HFInvMassFitter.cxx" runMassFitter.C\(\"${CUR_CFG}\"\)
 done
