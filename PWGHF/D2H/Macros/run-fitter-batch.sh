d49e761418659aa6f5e61010567b4bffcb0b7c04
saganatt <8majak8@gmail.com>
2024-06-19 20:27:04 +0200

    Upgrade fitter scripts and config

--- PWGHF/D2H/Macros/run-fitter-batch.sh
+++ PWGHF/D2H/Macros/run-fitter-batch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-INPUT_DIR="/data8/majak/MLHEP/input-d2h-fitter"
+INPUT_DIR="/data8/majak/MLHEP/input-d2h-fitter-fdd"
 INPUT_PATTERN="${INPUT_DIR}/projections_"
 
 CONFIG="config_massfitter"
@@ -11,20 +11,27 @@ for dir in ${INPUT_PATTERN}* ; do
 
   suffix=${dir##${INPUT_PATTERN}}
   echo $suffix
-  
+
   ROOT_EXT=".root"
   suffix_no_ext=${suffix%%${ROOT_EXT}}
   echo $suffix_no_ext
 
-  RESDIR="/data8/majak/d2h-fitter/100624/results-probs-bkg_${suffix_no_ext}"
-  mkdir "${RESDIR}"
+  RESDIR="results-fdd_${suffix_no_ext}"
+  RESPATH="/data8/majak/d2h-fitter/100624/${RESDIR}"
+  mkdir "${RESPATH}"
 
   CUR_CFG="${CONFIG}_${suffix_no_ext}_edit.json"
-  cp "${CONFIG_EXT}" "${CUR_CFG}" 
+  cp "${CONFIG_EXT}" "${CUR_CFG}"
+
+  sed -i "s/%indir%/${INPUT_DIR//\//\\/}/g" "${CUR_CFG}" || exit 1
   sed -i "s/%infile%/projections_${suffix}/g" "${CUR_CFG}" || exit 1
 
-  sed -i "s/%outdir%/results-probs-bkg_${suffix_no_ext}/g" "${CUR_CFG}" || exit 1
+  sed -i "s/%outdir%/${RESPATH//\//\\/}/g" "${CUR_CFG}" || exit 1
   sed -i "s/%sf%/${suffix_no_ext}/g" "${CUR_CFG}" || exit 1
 
   root -b -l -q -x "HFInvMassFitter.cxx" runMassFitter.C\(\"${CUR_CFG}\"\)
 done
+
+# ${RESPATH//\//\\/} is parameter expansion with additional parameters pattern and string: ${parameter/pattern/string}
+# Pattern is /\/, starting / means that all occurences of the pattern should be replaced
+# String is \\/, i.e., \/ in the final RESPATH -- slash escaped
