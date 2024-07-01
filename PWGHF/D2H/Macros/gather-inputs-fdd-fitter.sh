fc9f6851994ef5cb4697d7bfa8d6bf14d73dee97
saganatt <8majak8@gmail.com>
2024-07-01 18:20:38 +0200

    Save the 10-63 dense config for 12-24 with sigma 230 and problematic step at 20s

--- PWGHF/D2H/Macros/gather-inputs-fdd-fitter.sh
+++ PWGHF/D2H/Macros/gather-inputs-fdd-fitter.sh
@@ -3,7 +3,7 @@
 MLHEP_DIR="/data8/majak/MLHEP"
 OUTPUT_DIR="${MLHEP_DIR}/input-fd_batch_0624/d2h-fitter-sigmas3"
 
-RESDIR_PATTERN="/data8/majak/d2h-fitter/100624/results-fdd-refinedbkg2_sigmas_0.0100_0.0110_0.0136_0.0157_0.0200_0.0220_"
+RESDIR_PATTERN="/data8/majak/d2h-fitter/100624/results-fdd-refinedbkg2_sigmas_0.0100_0.0110_0.0136_0.0157_0.0190_0.0230_"
 
 for dir in ${RESDIR_PATTERN}* ; do
   suffix=${dir##${RESDIR_PATTERN}}
