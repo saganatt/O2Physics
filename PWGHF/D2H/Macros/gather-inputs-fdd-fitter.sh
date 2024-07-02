7955356eaaa540bd4e20f106e61767ac0e8ab529
saganatt <8majak8@gmail.com>
2024-07-02 13:04:26 +0200

    Config for fd precise

--- PWGHF/D2H/Macros/gather-inputs-fdd-fitter.sh
+++ PWGHF/D2H/Macros/gather-inputs-fdd-fitter.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 MLHEP_DIR="/data8/majak/MLHEP"
-OUTPUT_DIR="${MLHEP_DIR}/input-fd_batch_0624/d2h-fitter-sigmas3"
+OUTPUT_DIR="${MLHEP_DIR}/input-fd_precise_0624/d2h-fitter"
 
-RESDIR_PATTERN="/data8/majak/d2h-fitter/100624/results-fdd-refinedbkg2_sigmas_0.0100_0.0110_0.0136_0.0157_0.0190_0.0230_"
+RESDIR_PATTERN="/data8/majak/d2h-fitter/100624/results-fdd-precise_"
 
 for dir in ${RESDIR_PATTERN}* ; do
   suffix=${dir##${RESDIR_PATTERN}}
