6435e7a5a346397b6688fb11f33ae8643832c32d
saganatt <8majak8@gmail.com>
2024-07-23 15:07:48 +0200

    Adjust scripts and config to hyp data

--- PWGHF/D2H/Macros/gather-inputs-fdd-fitter.sh
+++ PWGHF/D2H/Macros/gather-inputs-fdd-fitter.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 MLHEP_DIR="/data8/majak/MLHEP"
-OUTPUT_DIR="${MLHEP_DIR}/input-fd_precise_0624/d2h-fitter"
+OUTPUT_DIR="${MLHEP_DIR}/input-fd_precise_2224/d2h-fitter"
 
-RESDIR_PATTERN="/data8/majak/d2h-fitter/100624/results-fdd-precise_"
+RESDIR_PATTERN="/data8/majak/d2h-fitter/220724/results-hyp-ml"
 
 for dir in ${RESDIR_PATTERN}* ; do
   suffix=${dir##${RESDIR_PATTERN}}
