ef4408b43f13205a3816ec3ebda87604d34d8c8a
saganatt <8majak8@gmail.com>
2024-08-01 21:29:02 +0200

    More trials with 8-12 and 12-24

--- PWGHF/D2H/Macros/gather-input-fitter.sh
+++ PWGHF/D2H/Macros/gather-input-fitter.sh
@@ -3,13 +3,13 @@
 MLHEP_DIR="/data8/majak/MLHEP"
 OUTPUT_DIR="/data8/majak/MLHEP/input-d2h-fitter-hyp-ml-fdd"
 
-RESDIR_PATTERN="${MLHEP_DIR}/results-2207-hyp-ml_fd_precise_bin23_"
+RESDIR_PATTERN="${MLHEP_DIR}/results-2207-hyp-ml_fd_precise_bin23_bkg_0.10"
 
 for dir in ${RESDIR_PATTERN}* ; do
   suffix=${dir##${RESDIR_PATTERN}}
   echo $suffix
   
-  RESPATH="${OUTPUT_DIR}/projections_bin23_${suffix}.root"
+  RESPATH="${OUTPUT_DIR}/projections_bin23_bkg_0.10${suffix}.root"
 
   python gather_input_fitter.py \
     "${dir}/LHC22pp/Results/resultsdatatot/masshisto.root" \
