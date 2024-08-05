716945ec210c3aee1a09c2e61ab76f1f7e1b444a
saganatt <8majak8@gmail.com>
2024-08-05 08:12:36 +0200

    Added combined yields to FDD input

--- PWGHF/D2H/Macros/gather-inputs-fdd-fitter.sh
+++ PWGHF/D2H/Macros/gather-inputs-fdd-fitter.sh
@@ -3,11 +3,11 @@
 MLHEP_DIR="/data8/majak/MLHEP"
 OUTPUT_DIR="${MLHEP_DIR}/input-fd_precise_2224/d2h-fitter"
 
-RESDIR_PATTERN="/data8/majak/d2h-fitter/220724/results-hyp-ml"
+RESDIR_PATTERN="/data8/majak/d2h-fitter/220724/results-hyp-ml_fdd_precise_"
 
-for dir in ${RESDIR_PATTERN}*-bin23-fixed-sigma ; do
+for dir in ${RESDIR_PATTERN}* ; do
   suffix=${dir##${RESDIR_PATTERN}}
   echo $suffix
 
-  cp "${dir}/fits.root" "${OUTPUT_DIR}/yields-fd${suffix}.root"
+  cp "${dir}/fits.root" "${OUTPUT_DIR}/yields-${suffix}.root"
 done
