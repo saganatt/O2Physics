0339f6318097752502b71809eae3a08b1635a146
saganatt <8majak8@gmail.com>
2024-08-06 18:12:40 +0200

    Update gather script

--- PWGHF/D2H/Macros/gather-input-fitter.sh
+++ PWGHF/D2H/Macros/gather-input-fitter.sh
@@ -3,13 +3,13 @@
 MLHEP_DIR="/data8/majak/MLHEP"
 OUTPUT_DIR="/data8/majak/MLHEP/input-d2h-fitter-hyp-ml-fdd"
 
-RESDIR_PATTERN="${MLHEP_DIR}/results-2207-hyp-ml_fd_precise_bin23_bkg_0.10"
+RESDIR_PATTERN="${MLHEP_DIR}/results-2207-hyp-ml_fd_precise_bkg_0.20"
 
 for dir in ${RESDIR_PATTERN}* ; do
   suffix=${dir##${RESDIR_PATTERN}}
   echo $suffix
   
-  RESPATH="${OUTPUT_DIR}/projections_bin23_bkg_0.10${suffix}.root"
+  RESPATH="${OUTPUT_DIR}/projections_bkg_0.20${suffix}.root"
 
   python gather_input_fitter.py \
     "${dir}/LHC22pp/Results/resultsdatatot/masshisto.root" \
