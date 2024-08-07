cb3e3f7e28e6a3c88d65499682ca655ed89ce26a
saganatt <8majak8@gmail.com>
2024-08-07 20:56:26 +0200

    Config for new batch run for 8-12, 12-24 with new bkg cuts

--- PWGHF/D2H/Macros/gather-input-fitter.sh
+++ PWGHF/D2H/Macros/gather-input-fitter.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 MLHEP_DIR="/data8/majak/MLHEP"
-OUTPUT_DIR="/data8/majak/MLHEP/input-d2h-fitter-hyp-ml-fdd"
+OUTPUT_DIR="/data8/majak/MLHEP/input-d2h-fitter-hyp-ml-fdd-812"
 
-RESDIR_PATTERN="${MLHEP_DIR}/results-2207-hyp-ml_fd_precise_bkg_0.20"
+RESDIR_PATTERN="${MLHEP_DIR}/results-2207-hyp-ml_fd_batch_"
 
 for dir in ${RESDIR_PATTERN}* ; do
   suffix=${dir##${RESDIR_PATTERN}}
   echo $suffix
   
-  RESPATH="${OUTPUT_DIR}/projections_bkg_0.20${suffix}.root"
+  RESPATH="${OUTPUT_DIR}/projections_${suffix}.root"
 
   python gather_input_fitter.py \
     "${dir}/LHC22pp/Results/resultsdatatot/masshisto.root" \
