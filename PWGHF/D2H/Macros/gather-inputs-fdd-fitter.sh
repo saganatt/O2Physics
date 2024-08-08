bb5cab456e176acf0c4ec06f30676b2f4c300a83
saganatt <8majak8@gmail.com>
2024-08-08 15:18:43 +0200

    Config for 8-12 and 12-24 new bkg

--- PWGHF/D2H/Macros/gather-inputs-fdd-fitter.sh
+++ PWGHF/D2H/Macros/gather-inputs-fdd-fitter.sh
@@ -3,9 +3,9 @@
 MLHEP_DIR="/data8/majak/MLHEP"
 OUTPUT_DIR="${MLHEP_DIR}/input-fd_precise_2224/d2h-fitter"
 
-RESDIR_PATTERN="/data8/majak/d2h-fitter/220724/results-hyp-ml_fdd_precise_"
+RESDIR_PATTERN="/data8/majak/d2h-fitter/220724/results-hyp-ml_bkg_"
 
-for dir in ${RESDIR_PATTERN}* ; do
+for dir in ${RESDIR_PATTERN}*-0708-checks-fixed-sigma ; do
   suffix=${dir##${RESDIR_PATTERN}}
   echo $suffix
 
