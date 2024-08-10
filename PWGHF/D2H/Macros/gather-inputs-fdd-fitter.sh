198e5e5d4d1d065d0b1bebed7250e84d1211c95e
saganatt <8majak8@gmail.com>
2024-08-10 02:16:54 +0200

    Back to fdd precise for systematics. Without 8-12 and 12-24

--- PWGHF/D2H/Macros/gather-inputs-fdd-fitter.sh
+++ PWGHF/D2H/Macros/gather-inputs-fdd-fitter.sh
@@ -3,9 +3,9 @@
 MLHEP_DIR="/data8/majak/MLHEP"
 OUTPUT_DIR="${MLHEP_DIR}/input-fd_precise_2224/d2h-fitter"
 
-RESDIR_PATTERN="/data8/majak/d2h-fitter/220724/results-hyp-ml_bkg_"
+RESDIR_PATTERN="/data8/majak/d2h-fitter/220724/results-hyp-ml_fdd_precise_"
 
-for dir in ${RESDIR_PATTERN}*-0708-checks-fixed-sigma ; do
+for dir in ${RESDIR_PATTERN}* ; do
   suffix=${dir##${RESDIR_PATTERN}}
   echo $suffix
 
