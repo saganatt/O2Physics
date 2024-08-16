b2a9adacbbb4f7522d2aa2ee0b39a76b6727176a
saganatt <8majak8@gmail.com>
2024-08-16 13:45:39 +0200

    Configs for track tuner systematics

--- PWGHF/D2H/Macros/gather-inputs-fdd-fitter.sh
+++ PWGHF/D2H/Macros/gather-inputs-fdd-fitter.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 MLHEP_DIR="/data8/majak/MLHEP"
-OUTPUT_DIR="${MLHEP_DIR}/input-fd_batch_0824/d2h-fitter"
+OUTPUT_DIR="${MLHEP_DIR}/input-fd_precise_0824/d2h-fitter"
 
 RESDIR_PATTERN="/data8/majak/d2h-fitter/140824/results-hyp-ml_"
 
-for dir in ${RESDIR_PATTERN}bkg_0.10* ; do
+for dir in ${RESDIR_PATTERN}fdd_precise_* ; do
   suffix=${dir##${RESDIR_PATTERN}}
   echo $suffix
 
