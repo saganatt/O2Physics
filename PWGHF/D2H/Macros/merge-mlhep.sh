7b11d71a8d34e796356e9bf45ff2a6eacde41bd8
saganatt <8majak8@gmail.com>
2024-08-16 13:23:44 +0200

    Config for running track tuner systematics

--- PWGHF/D2H/Macros/merge-mlhep.sh
+++ PWGHF/D2H/Macros/merge-mlhep.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
 MLHEP_DIR="/data8/majak/MLHEP"
-OUTPUT_DIR="/data8/majak/MLHEP/input-d2h-fitter-hyp-ml-fdd-precise-merged"
 
-FD_12=(0.26 0.28 0.30 0.32 0.34 0.36 0.38 0.40 0.42 0.44 0.46 0.48 0.50 0.52 0.55 0.58 0.60)
-FD_23=(0.20 0.22 0.24 0.26 0.28 0.30 0.32 0.34 0.36 0.38 0.40 0.42 0.44 0.46 0.48 0.50 0.52)
-FD_34=(0.26 0.28 0.30 0.32 0.34 0.36 0.38 0.40 0.42 0.44 0.46 0.48 0.50 0.52 0.55 0.58 0.60)
-FD_45=(0.17 0.19 0.21 0.23 0.25 0.27 0.29 0.31 0.33 0.35 0.38 0.40 0.43 0.47 0.50 0.54 0.58)
-FD_56=(0.12 0.14 0.16 0.18 0.20 0.22 0.24 0.26 0.27 0.30 0.32 0.34 0.36 0.38 0.41 0.45 0.49)
-FD_68=(0.16 0.18 0.20 0.22 0.24 0.26 0.28 0.30 0.32 0.34 0.36 0.38 0.40 0.42 0.46 0.48 0.52)
-FD_812=(0.07 0.11 0.13 0.15 0.17 0.18 0.21 0.22 0.25 0.27 0.28 0.30 0.34 0.37 0.41 0.45 0.48)
-FD_1224=(0.07 0.11 0.13 0.15 0.17 0.18 0.21 0.22 0.25 0.27 0.28 0.30 0.34 0.37 0.41 0.45 0.48)
+FD_12=(0.00 0.26 0.28 0.30 0.32 0.34 0.36 0.38 0.40 0.42 0.44 0.46 0.48 0.50 0.52 0.55 0.58 0.60)
+FD_23=(0.00 0.20 0.22 0.24 0.26 0.28 0.30 0.32 0.34 0.36 0.38 0.40 0.42 0.44 0.46 0.48 0.50 0.52)
+FD_34=(0.00 0.26 0.28 0.30 0.32 0.34 0.36 0.38 0.40 0.42 0.44 0.46 0.48 0.50 0.52 0.55 0.58 0.60)
+FD_45=(0.00 0.17 0.19 0.21 0.23 0.25 0.27 0.29 0.31 0.33 0.35 0.38 0.40 0.43 0.47 0.50 0.54 0.58)
+FD_56=(0.00 0.12 0.14 0.16 0.18 0.20 0.22 0.24 0.26 0.27 0.30 0.32 0.34 0.36 0.38 0.41 0.45 0.49)
+FD_68=(0.00 0.16 0.18 0.20 0.22 0.24 0.26 0.28 0.30 0.32 0.34 0.36 0.38 0.40 0.42 0.46 0.48 0.52)
+FD_812=(0.00 0.07 0.11 0.13 0.15 0.17 0.18 0.21 0.22 0.25 0.27 0.28 0.30 0.34 0.37 0.41 0.45 0.48)
+FD_1224=(0.00 0.07 0.11 0.13 0.15 0.17 0.18 0.21 0.22 0.25 0.27 0.28 0.30 0.34 0.37 0.41 0.45 0.48)
 
-BKG_812=0.20
-BKG_1224=0.30
+BKG_812=0.10
+BKG_1224=0.70
 
 for i in "${!FD_12[@]}" ; do
   fd12=${FD_12[i]}
@@ -25,11 +24,11 @@ for i in "${!FD_12[@]}" ; do
   fd812=${FD_812[i]}
   fd1224=${FD_1224[i]}
   
-  RESULTS_23="${MLHEP_DIR}/results-2207-hyp-ml_fd_precise_bkg_${BKG_812}_${BKG_1224}_fd_${fd12}_${fd23}_${fd45}_${fd68}_${fd812}_${fd1224}"
-  RESULTS_34="${MLHEP_DIR}/results-2207-hyp-ml_fd_precise_bkg_${BKG_812}_${BKG_1224}_fd_${fd12}_${fd34}_${fd56}_${fd68}_${fd812}_${fd1224}"
+  RESULTS_23="${MLHEP_DIR}/results-1408-hyp-ml_fd_precise_no_pt_smearing_bkg_${BKG_812}_${BKG_1224}_fd_${fd12}_${fd23}_${fd45}_${fd68}_${fd812}_${fd1224}"
+  RESULTS_34="${MLHEP_DIR}/results-1408-hyp-ml_fd_precise_no_pt_smearing_bkg_${BKG_812}_${BKG_1224}_fd_${fd12}_${fd34}_${fd56}_${fd68}_${fd812}_${fd1224}"
 
-  OUTPUT_DIR="/data8/majak/MLHEP/input-d2h-fitter-hyp-ml-fdd-precise-merged"
-  RESPATH="${OUTPUT_DIR}/projections_fdd_precise_bkg_${BKG_812}_${BKG_1224}_fd_${fd12}_${fd23}_${fd34}_${fd45}_${fd56}_${fd68}_${fd812}_${fd1224}.root"
+  OUTPUT_DIR="/data8/majak/MLHEP/input-d2h-fitter-140824-fdd-precise-merged"
+  RESPATH="${OUTPUT_DIR}/projections_fdd_precise_no_pt_smearing_bkg_${BKG_812}_${BKG_1224}_fd_${fd12}_${fd23}_${fd34}_${fd45}_${fd56}_${fd68}_${fd812}_${fd1224}.root"
 
   python merge_histomass.py \
     -n hmassfPt \
@@ -43,19 +42,19 @@ for i in "${!FD_12[@]}" ; do
        "${RESULTS_23}/LHC22pp/Results/resultsdatatot/masshisto.root" \
        "${RESULTS_23}/LHC22pp/Results/resultsdatatot/masshisto.root"
 
-  OUTPUT_DIR="/data8/majak/MLHEP/input-fd_precise_2224"
-  RESPATH="${OUTPUT_DIR}/efficienciesLcpKpiRun3analysis_bkg_${BKG_812}_${BKG_1224}_fd_${fd12}_${fd23}_${fd34}_${fd45}_${fd56}_${fd68}_${fd812}_${fd1224}.root"
+  OUTPUT_DIR="/data8/majak/MLHEP/input-fd_precise_0824"
+  RESPATH="${OUTPUT_DIR}/efficienciesLcpKpiRun3analysis_no_pt_smearing_bkg_${BKG_812}_${BKG_1224}_fd_${fd12}_${fd23}_${fd34}_${fd45}_${fd56}_${fd68}_${fd812}_${fd1224}.root"
 
   python merge_eff.py \
     -n eff \
     -n eff_fd \
     -o ${RESPATH} \
-    -i "${RESULTS_23}/LHC22pp_mc/Results/prod_LHC24d3b/resultsmctot/efficienciesLcpKpiRun3analysis.root" \
-       "${RESULTS_23}/LHC22pp_mc/Results/prod_LHC24d3b/resultsmctot/efficienciesLcpKpiRun3analysis.root" \
-       "${RESULTS_34}/LHC22pp_mc/Results/prod_LHC24d3b/resultsmctot/efficienciesLcpKpiRun3analysis.root" \
-       "${RESULTS_23}/LHC22pp_mc/Results/prod_LHC24d3b/resultsmctot/efficienciesLcpKpiRun3analysis.root" \
-       "${RESULTS_34}/LHC22pp_mc/Results/prod_LHC24d3b/resultsmctot/efficienciesLcpKpiRun3analysis.root" \
-       "${RESULTS_23}/LHC22pp_mc/Results/prod_LHC24d3b/resultsmctot/efficienciesLcpKpiRun3analysis.root" \
-       "${RESULTS_23}/LHC22pp_mc/Results/prod_LHC24d3b/resultsmctot/efficienciesLcpKpiRun3analysis.root" \
-       "${RESULTS_23}/LHC22pp_mc/Results/prod_LHC24d3b/resultsmctot/efficienciesLcpKpiRun3analysis.root"
+    -i "${RESULTS_23}/LHC24pp_mc/Results/prod_LHC24d3b/resultsmctot/efficienciesLcpKpiRun3analysis.root" \
+       "${RESULTS_23}/LHC24pp_mc/Results/prod_LHC24d3b/resultsmctot/efficienciesLcpKpiRun3analysis.root" \
+       "${RESULTS_34}/LHC24pp_mc/Results/prod_LHC24d3b/resultsmctot/efficienciesLcpKpiRun3analysis.root" \
+       "${RESULTS_23}/LHC24pp_mc/Results/prod_LHC24d3b/resultsmctot/efficienciesLcpKpiRun3analysis.root" \
+       "${RESULTS_34}/LHC24pp_mc/Results/prod_LHC24d3b/resultsmctot/efficienciesLcpKpiRun3analysis.root" \
+       "${RESULTS_23}/LHC24pp_mc/Results/prod_LHC24d3b/resultsmctot/efficienciesLcpKpiRun3analysis.root" \
+       "${RESULTS_23}/LHC24pp_mc/Results/prod_LHC24d3b/resultsmctot/efficienciesLcpKpiRun3analysis.root" \
+       "${RESULTS_23}/LHC24pp_mc/Results/prod_LHC24d3b/resultsmctot/efficienciesLcpKpiRun3analysis.root"
 done
--- PWGHF/D2H/Macros/run-fitter-batch.sh
+++ PWGHF/D2H/Macros/run-fitter-batch.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-INPUT_DIR="/data8/majak/MLHEP/input-d2h-fitter-140824-fdd"
+INPUT_DIR="/data8/majak/MLHEP/input-d2h-fitter-140824-fdd-precise-merged"
 INPUT_PATTERN="${INPUT_DIR}/projections_"
 
 CONFIG="config_massfitter"
 CONFIG_EXT="${CONFIG}.json"
 
-for dir in ${INPUT_PATTERN}bkg_0.10* ; do
+for dir in ${INPUT_PATTERN}*no_pt_smearing* ; do
   #dir=( ${INPUT_PATTERN}*${fd}.root )
   echo $dir
 
@@ -18,10 +18,10 @@ for dir in ${INPUT_PATTERN}bkg_0.10* ; do
   echo $suffix_no_ext
 
   IFS='_' read -ra probs <<< "${suffix_no_ext}"
-  echo "bkg ${probs[1]} ${probs[2]}"
-  echo "fd ${probs[4]} ${probs[5]} ${probs[6]} ${probs[7]} ${probs[8]} ${probs[9]} ${probs[10]} ${probs[11]}"
+  echo "bkg ${probs[6]} ${probs[7]}"
+  echo "fd ${probs[9]} ${probs[10]} ${probs[11]} ${probs[12]} ${probs[13]} ${probs[14]} ${probs[15]} ${probs[16]}"
 
-  RESDIR="results-hyp-ml_${suffix_no_ext}"
+  RESDIR="results-hyp-ml_${suffix_no_ext}-old-mlhep-range-sigma155"
   RESPATH="/data8/majak/d2h-fitter/140824/${RESDIR}"
   mkdir "${RESPATH}"
 
@@ -32,17 +32,17 @@ for dir in ${INPUT_PATTERN}bkg_0.10* ; do
   sed -i "s/%infile%/projections_${suffix}/g" "${CUR_CFG}" || exit 1
   sed -i "s/%outdir%/${RESPATH//\//\\/}/g" "${CUR_CFG}" || exit 1
 
-  sed -i "s/%bkg812%/${probs[1]}/g" "${CUR_CFG}" || exit 1
-  sed -i "s/%bkg1224%/${probs[2]}/g" "${CUR_CFG}" || exit 1
+  sed -i "s/%bkg812%/${probs[6]}/g" "${CUR_CFG}" || exit 1
+  sed -i "s/%bkg1224%/${probs[7]}/g" "${CUR_CFG}" || exit 1
 
-  sed -i "s/%sf12%/${probs[4]}/g" "${CUR_CFG}" || exit 1
-  sed -i "s/%sf23%/${probs[4]}/g" "${CUR_CFG}" || exit 1
-  sed -i "s/%sf34%/${probs[4]}/g" "${CUR_CFG}" || exit 1
-  sed -i "s/%sf45%/${probs[4]}/g" "${CUR_CFG}" || exit 1
-  sed -i "s/%sf56%/${probs[4]}/g" "${CUR_CFG}" || exit 1
-  sed -i "s/%sf68%/${probs[4]}/g" "${CUR_CFG}" || exit 1
-  sed -i "s/%sf812%/${probs[4]}/g" "${CUR_CFG}" || exit 1
-  sed -i "s/%sf1224%/${probs[4]}/g" "${CUR_CFG}" || exit 1
+  sed -i "s/%sf12%/${probs[9]}/g" "${CUR_CFG}" || exit 1
+  sed -i "s/%sf23%/${probs[10]}/g" "${CUR_CFG}" || exit 1
+  sed -i "s/%sf34%/${probs[11]}/g" "${CUR_CFG}" || exit 1
+  sed -i "s/%sf45%/${probs[12]}/g" "${CUR_CFG}" || exit 1
+  sed -i "s/%sf56%/${probs[13]}/g" "${CUR_CFG}" || exit 1
+  sed -i "s/%sf68%/${probs[14]}/g" "${CUR_CFG}" || exit 1
+  sed -i "s/%sf812%/${probs[15]}/g" "${CUR_CFG}" || exit 1
+  sed -i "s/%sf1224%/${probs[16]}/g" "${CUR_CFG}" || exit 1
 
   root -b -l -q -x "HFInvMassFitter.cxx" runMassFitter.C\(\"${CUR_CFG}\"\)
 done
