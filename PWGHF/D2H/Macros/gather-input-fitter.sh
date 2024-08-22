e6e0691fa96a78d4570f2a0a3b85061b68d73f40
saganatt <8majak8@gmail.com>
2024-08-22 16:59:13 +0200

    Put all changing variables at the top of gather-input-fitter

--- PWGHF/D2H/Macros/gather-input-fitter.sh
+++ PWGHF/D2H/Macros/gather-input-fitter.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 MLHEP_DIR="/data8/majak/MLHEP"
-OUTPUT_DIR="/data8/majak/MLHEP/input-d2h-fitter-140824-fdd-precise"
+OUTPUT_DIR="/data8/majak/MLHEP/input-d2h-fitter-22082024"
 
-RESDIR_PATTERN="${MLHEP_DIR}/results-1408-hyp-ml_"
+RESDIR_PATTERN="${MLHEP_DIR}/results-2208-hyp-ml_"
+PERM_PATTERN="fd_precise_"
 
-for dir in ${RESDIR_PATTERN}fd_precise_finer_bins* ; do
+for dir in ${RESDIR_PATTERN}${PERM_PATTERN}* ; do
   suffix=${dir##${RESDIR_PATTERN}}
   echo $suffix
   
