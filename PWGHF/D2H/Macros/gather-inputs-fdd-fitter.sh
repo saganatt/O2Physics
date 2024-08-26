96d2c4829a142c8ab4d926934ceb6e6d0b5e051f
saganatt <8majak8@gmail.com>
2024-08-26 11:37:32 +0200

    Update run scripts and config for mass fitter

--- PWGHF/D2H/Macros/gather-inputs-fdd-fitter.sh
+++ PWGHF/D2H/Macros/gather-inputs-fdd-fitter.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 MLHEP_DIR="/data8/majak/MLHEP"
-OUTPUT_DIR="${MLHEP_DIR}/input-fd-precise-22082024"
+OUTPUT_DIR="${MLHEP_DIR}/input-fd-precise-2308"
 
-RESDIR_PATTERN="/data8/majak/d2h-fitter/220824/results-hyp-ml_"
+RESDIR_PATTERN="/data8/majak/d2h-fitter/230824/results-hyp-ml_"
+PERM_PATTERN="fixed-sigma"
 
-for dir in ${RESDIR_PATTERN}*-rebin+1 ; do
+for dir in ${RESDIR_PATTERN}*${PERM_PATTERN} ; do
   suffix=${dir##${RESDIR_PATTERN}}
   echo $suffix
 
