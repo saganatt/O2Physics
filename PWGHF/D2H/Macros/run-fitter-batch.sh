77e66958521c26020d55fd7f2331c58c6e2d9a18
saganatt <8majak8@gmail.com>
2024-08-28 11:26:57 +0200

    Massfitter with best fixed sigma for 8-12

--- PWGHF/D2H/Macros/run-fitter-batch.sh
+++ PWGHF/D2H/Macros/run-fitter-batch.sh
@@ -7,7 +7,7 @@ CONFIG="config_massfitter"
 CONFIG_EXT="${CONFIG}.json"
 PERM_PATTERN="bkg_"
 
-RES_SUFFIX="-fixed-sigma-200"
+RES_SUFFIX="-fixed-sigma-191-68-155"
 
 for dir in ${INPUT_PATTERN}${PERM_PATTERN}* ; do
   #dir=( ${INPUT_PATTERN}*${fd}.root )
