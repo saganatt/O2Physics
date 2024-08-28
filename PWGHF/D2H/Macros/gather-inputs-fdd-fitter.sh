60226b0a18984320d6e49481b7264aaa1b81094c
saganatt <8majak8@gmail.com>
2024-08-28 11:26:57 +0200

    Massfitter with best fixed sigma for 8-12

--- PWGHF/D2H/Macros/gather-inputs-fdd-fitter.sh
+++ PWGHF/D2H/Macros/gather-inputs-fdd-fitter.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 MLHEP_DIR="/data8/majak/MLHEP"
-OUTPUT_DIR="${MLHEP_DIR}/input-fd-precise-2308"
+OUTPUT_DIR="${MLHEP_DIR}/input-fd-23082024"
 
 RESDIR_PATTERN="/data8/majak/d2h-fitter/230824/results-hyp-ml_"
-PERM_PATTERN="fixed-sigma"
+PERM_PATTERN="fixed-sigma-191-68-155"
 
 for dir in ${RESDIR_PATTERN}*${PERM_PATTERN} ; do
   suffix=${dir##${RESDIR_PATTERN}}
