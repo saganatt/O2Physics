727066264640cdf97c0f951a591d808563de751a
saganatt <8majak8@gmail.com>
2024-09-06 20:59:54 +0200

    Final paths in the run scripts

--- PWGHF/D2H/Macros/gather-inputs-fdd-fitter.sh
+++ PWGHF/D2H/Macros/gather-inputs-fdd-fitter.sh
@@ -4,7 +4,7 @@ MLHEP_DIR="/data8/majak/MLHEP"
 OUTPUT_DIR="${MLHEP_DIR}/input-fd-23082024"
 
 RESDIR_PATTERN="/data8/majak/d2h-fitter/230824/results-hyp-ml_"
-PERM_PATTERN="-wide4-fixed-sigma"
+PERM_PATTERN="-rebin4-double-gauss-fixed-sigma"
 
 for dir in ${RESDIR_PATTERN}*${PERM_PATTERN} ; do
   suffix=${dir##${RESDIR_PATTERN}}
