264e78476435ace336afb2a5d67d795e7fc7dd7f
saganatt <8majak8@gmail.com>
2024-09-12 10:32:12 +0200

    Mass fitter config and scripts for 1224 split precise

--- PWGHF/D2H/Macros/run-fitter-batch.sh
+++ PWGHF/D2H/Macros/run-fitter-batch.sh
@@ -5,7 +5,7 @@ INPUT_PATTERN="${INPUT_DIR}/projections_"
 
 CONFIG="config_massfitter"
 CONFIG_EXT="${CONFIG}.json"
-PERM_PATTERN="1224_split_widerange_"
+PERM_PATTERN="fd_precise_1224_split_"
 
 RES_SUFFIX="-fixed-sigma"
 
@@ -24,7 +24,7 @@ for dir in ${INPUT_PATTERN}${PERM_PATTERN}* ; do
 
   IFS='_' read -ra probs <<< "${suffix_no_ext_ext}"
   echo "bkg ${probs[1]} ${probs[2]}"
-  echo "fd ${probs[4]} ${probs[5]} ${probs[6]} ${probs[7]} ${probs[8]} ${probs[9]} ${probs[10]} ${probs[11]}"
+  echo "fd ${probs[4]} ${probs[5]} ${probs[6]} ${probs[7]} ${probs[8]} ${probs[9]} ${probs[10]} ${probs[11]} ${probs[12]}"
 
   RESDIR="results-hyp-ml_${suffix_no_ext}${RES_SUFFIX}"
   RESPATH="/data8/majak/d2h-fitter/100924/${RESDIR}"
@@ -41,14 +41,14 @@ for dir in ${INPUT_PATTERN}${PERM_PATTERN}* ; do
   sed -i "s/%bkg1624%/${probs[2]}/g" "${CUR_CFG}" || exit 1
 
   sed -i "s/%sf12%/${probs[4]}/g" "${CUR_CFG}" || exit 1
-  sed -i "s/%sf23%/${probs[4]}/g" "${CUR_CFG}" || exit 1
-  sed -i "s/%sf34%/${probs[4]}/g" "${CUR_CFG}" || exit 1
-  sed -i "s/%sf45%/${probs[4]}/g" "${CUR_CFG}" || exit 1
-  sed -i "s/%sf56%/${probs[4]}/g" "${CUR_CFG}" || exit 1
-  sed -i "s/%sf68%/${probs[4]}/g" "${CUR_CFG}" || exit 1
-  sed -i "s/%sf812%/${probs[4]}/g" "${CUR_CFG}" || exit 1
-  sed -i "s/%sf1216%/${probs[4]}/g" "${CUR_CFG}" || exit 1
-  sed -i "s/%sf1624%/${probs[4]}/g" "${CUR_CFG}" || exit 1
+  sed -i "s/%sf23%/${probs[5]}/g" "${CUR_CFG}" || exit 1
+  sed -i "s/%sf34%/${probs[6]}/g" "${CUR_CFG}" || exit 1
+  sed -i "s/%sf45%/${probs[7]}/g" "${CUR_CFG}" || exit 1
+  sed -i "s/%sf56%/${probs[8]}/g" "${CUR_CFG}" || exit 1
+  sed -i "s/%sf68%/${probs[9]}/g" "${CUR_CFG}" || exit 1
+  sed -i "s/%sf812%/${probs[10]}/g" "${CUR_CFG}" || exit 1
+  sed -i "s/%sf1216%/${probs[11]}/g" "${CUR_CFG}" || exit 1
+  sed -i "s/%sf1624%/${probs[12]}/g" "${CUR_CFG}" || exit 1
 
   root -b -l -q -x "HFInvMassFitter.cxx" runMassFitter.C\(\"${CUR_CFG}\"\)
 done
