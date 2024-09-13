abf7c74fdda607b77c502d63ec66e0da0561fe69
saganatt <8majak8@gmail.com>
2024-09-13 09:11:32 +0200

    Version of remove_high_pt for Luigi's merged Lc

--- PWGHF/D2H/Macros/remove_high_pt.py
+++ PWGHF/D2H/Macros/remove_high_pt.py
@@ -38,13 +38,14 @@ def main():
         empty_bins = len(bins)
         for binn in range(first_bin, last_bin + 1):
             bins.append(hist.GetBinLowEdge(binn))
-        #last_bins = [16.0, 24.0]
-        #bins += last_bins
+        print(f"Hist bins {bins}")
+        last_bins = [24.0]
+        bins += last_bins
         hist2 = TH1F(args.histname, "", len(bins) - 1, array('d', bins))
         for binn in range(empty_bins, last_bin + 1):
             hist2.SetBinContent(binn + 1, hist.GetBinContent(binn + 1 - empty_bins))
             hist2.SetBinError(binn + 1, hist.GetBinError(binn + 1 - empty_bins))
-            print(f"Setting bin {binn + 1} low edge {hist2.GetBinLowEdge(binn + 1)} content to content from bin {binn + 1 - empty_bins}")
+            print(f"Setting bin {binn + 1} low edge {hist2.GetBinLowEdge(binn + 1)} up edge {hist2.GetXaxis().GetBinUpEdge(binn + 1)} content to content from bin {binn + 1 - empty_bins}: {hist2.GetBinContent(binn + 1)}")
         hist2.SetMarkerSize(hist.GetMarkerSize())
         hist2.SetMarkerColor(hist.GetMarkerColor())
         hist2.SetMarkerStyle(hist.GetMarkerStyle())
