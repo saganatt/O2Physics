fd8ddc7d3ba5b58147d84bb31b0b1875ed5041cf
saganatt <8majak8@gmail.com>
2024-09-18 12:54:46 +0200

    Rewrite systematic errors to TGraphErrors. Graphics polishing

--- PWGHF/D2H/Macros/compare_fractions.py
+++ PWGHF/D2H/Macros/compare_fractions.py
@@ -14,7 +14,7 @@ import os
 from ROOT import (  # pylint: disable=import-error,no-name-in-module
     TCanvas,
     TFile,
-    TH1F,
+    TGraphErrors,
     TLegend,
     TPaveText,
     gROOT,
@@ -32,14 +32,14 @@ from ROOT import (  # pylint: disable=import-error,no-name-in-module
     kYellow
 )
 
-COLORS=[kRed-3, kBlack, kAzure-7, kGreen+2, kOrange-3, kMagenta+1, kBlue, kRed-3, kTeal+3, kGreen, kAzure+8,
+COLORS=[kBlack, kRed-3, kBlack, kAzure-7, kGreen+2, kOrange-3, kMagenta+1, kBlue, kRed-3, kTeal+3, kGreen, kAzure+8,
         kYellow+3, kOrange-5, kMagenta+2, kBlue-6, kCyan+1, kGreen-6]
 MODELS_COLORS=[kGray+1, kOrange-3, kCyan-2, kRed-9, kAzure-9]
 MODELS_STYLES=[3245, 3250, 3244, 3254, 3209]
 
 
 def get_alice_text(alice_text_config):
-    alice_text = TPaveText(0.17, 0.74, 0.50, 0.84, "brNDC")
+    alice_text = TPaveText(0.17, 0.70, 0.50, 0.84, "brNDC")
     alice_text.SetTextFont(42)
     alice_text.SetTextSize(0.04)
     alice_text.SetBorderSize(0)
@@ -48,7 +48,7 @@ def get_alice_text(alice_text_config):
 
     alice_text.AddText("#scale[1.35]{ALICE Preliminary}")
     #alice_text.AddText("#scale[1.05]{pp #sqrt{s} = 13.6 TeV}")
-    alice_text.AddText(f"#scale[1.15]{{{alice_text_config}}}")
+    alice_text.AddText(f"#scale[1.20]{{{alice_text_config}}}")
 
     return alice_text
 
@@ -58,7 +58,7 @@ def get_legend(x_1, y_1, x_2, y_2, num_hists):
     if num_hists > 4:
         leg.SetNColumns(2)
     leg.SetTextAlign(12)
-    leg.SetTextSize(0.04)
+    leg.SetTextSize(0.045)
     leg.SetMargin(0.1)
     leg.SetBorderSize(0)
     leg.SetFillStyle(0)
@@ -66,7 +66,7 @@ def get_legend(x_1, y_1, x_2, y_2, num_hists):
 
 def prepare_canvas(cname):
     canv = TCanvas(cname, "")
-    canv.SetCanvasSize(800, 600)
+    canv.SetCanvasSize(900, 600)
     canv.SetTickx()
     canv.SetTicky()
     canv.SetLeftMargin(0.15)
@@ -90,14 +90,17 @@ def combine_syst_errors(syst_errors, value):
     return math.sqrt(err) * value
 
 
-def get_hist_limits(hist):
+def get_hist_limits(hist, graph_syst = None):
     miny = 0.0
     maxy = 0.0
     for binn in range(hist.GetNbinsX()):
         maxval = hist.GetBinContent(binn + 1) + hist.GetBinError(binn + 1)
         minval = hist.GetBinContent(binn + 1) - hist.GetBinError(binn + 1)
-        maxy = maxval if maxval > maxy else maxy
-        miny = minval if minval < miny else miny
+        if graph_syst:
+            maxval = max(maxval, hist.GetBinContent(binn + 1) + graph_syst.GetErrorY(binn))
+            minval = min(minval, hist.GetBinContent(binn + 1) - graph_syst.GetErrorY(binn))
+        maxy = max(maxval, maxy)
+        miny = min(minval, miny)
     return miny, maxy
 
 
@@ -106,22 +109,12 @@ def merge_fractions(inputdir, histname, filenames):
         reshist = fin.Get(histname).Clone()
         reshist.SetDirectory(0)
 
-    print(f"First hist bins:")
-    for binn in range(reshist.GetNbinsX()):
-        print(f"Bin {binn + 1} low edge {reshist.GetBinLowEdge(binn + 1)} up edge {reshist.GetXaxis().GetBinUpEdge(binn + 1)}")
-
     for ind, file in enumerate(filenames[1:]):
         ind += 1
         with TFile.Open(os.path.join(inputdir, file)) as fin:
             hist = fin.Get(histname)
             reshist.SetBinContent(ind + 1, hist.GetBinContent(ind + 1))
             reshist.SetBinError(ind + 1, hist.GetBinError(ind + 1))
-            print(f"Setting bin {ind + 1} low edge {reshist.GetBinLowEdge(ind + 1)} up edge {reshist.GetXaxis().GetBinUpEdge(ind + 1)} to {reshist.GetBinContent(ind + 1)}")
-
-            #maxy = max(hist.GetBinContent(ind + 1), maxy)
-            #miny = min(hist.GetBinContent(ind + 1), miny)
-    #reshist.SetMaximum(maxy)
-    #reshist.SetMinimum(miny)
 
     return reshist
 
@@ -137,6 +130,7 @@ def set_hist_style(hist, color, y_axis):
     hist.GetXaxis().SetLabelOffset(0.02)
     hist.SetMarkerColor(color)
     hist.SetLineColor(color)
+    hist.SetLineWidth(2)
     hist.GetXaxis().SetTitle("#it{p}_{T}(GeV/#it{c})")
     hist.GetYaxis().SetTitle(y_axis)
 
@@ -154,16 +148,22 @@ def get_hist_for_label(label, color, cfg):
     return hist
 
 
-def get_hist_systematics(hist, label, color, cfg):
-    hist_syst = hist.Clone(f"{label}_syst")
-    hist_syst.SetDirectory(0)
-    for binn in range(hist_syst.GetNbinsX()):
+def get_graph_systematics(hist, label, color, cfg):
+    graph_syst = TGraphErrors()
+    for binn in range(hist.GetNbinsX()):
         syst_err = combine_syst_errors(cfg["hists"][label]["systematics"][binn],
-                                           hist_syst.GetBinContent(binn + 1))
+                                           hist.GetBinContent(binn + 1))
         print(f"Syst error {label} bin {binn + 1} {syst_err}")
-        hist_syst.SetBinError(binn + 1, syst_err)
-    hist_syst.SetFillStyle(0)
-    return hist_syst
+        x_point = hist.GetBinCenter(binn + 1)
+        y_point = hist.GetBinContent(binn + 1)
+        x_width = hist.GetBinWidth(binn + 1) / 2.0
+        x_width /= 2.0 # We want syst boxes to be of half-bin width
+        if y_point != 0:
+            graph_syst.SetPoint(binn, x_point, y_point)
+            graph_syst.SetPointError(binn, x_width, syst_err)
+    set_hist_style(graph_syst, color, cfg["y_axis"])
+    graph_syst.SetFillStyle(0)
+    return graph_syst
 
 
 def get_hist_model(label, color, style, cfg):
@@ -201,12 +201,12 @@ def plot_compare(cfg):
 
             hists_models.append(hist)
     else:
-        #leg = get_legend(0.17, 0.58, 0.65, 0.74, len(cfg["hists"]))
-        leg = get_legend(0.33, 0.16, 0.87, 0.28, len(cfg["hists"]))
+        leg = get_legend(0.17, 0.58, 0.65, 0.70, len(cfg["hists"]))
+        #leg = get_legend(0.42, 0.18, 0.97, 0.32, len(cfg["hists"]))
         leg_models = None
 
     hists = {}
-    hists_syst = []
+    graphs_syst = []
     for ind, (label, color) in enumerate(zip(cfg["hists"], COLORS)):
         hist = get_hist_for_label(label, color, cfg)
         miny, maxy = get_hist_limits(hist)
@@ -220,10 +220,10 @@ def plot_compare(cfg):
 
         if cfg["hists"][label].get("systematics", None):
             print("Plotting systematic")
-            hist_syst = get_hist_systematics(hist, label, color, cfg)
-            miny, maxy = get_hist_limits(hist_syst)
-            hist_syst.Draw("sameE2")
-            hists_syst.append(hist_syst)
+            graph_syst = get_graph_systematics(hist, label, color, cfg)
+            miny, maxy = get_hist_limits(hist, graph_syst)
+            graph_syst.Draw("sameE2")
+            graphs_syst.append(graph_syst)
 
     margin = 0.05
     #k = 1.0 - 2 * margin
@@ -236,8 +236,8 @@ def plot_compare(cfg):
         hist_models.GetYaxis().SetRangeUser(miny, maxy + margin)
     for _, hist in hists.items():
         hist.GetYaxis().SetRangeUser(miny, maxy + margin)
-    for hist_syst in hists_syst:
-        hist_syst.GetYaxis().SetRangeUser(miny, maxy + margin)
+    for graph_syst in graphs_syst:
+        graph_syst.GetYaxis().SetRangeUser(miny, maxy + margin)
 
     leg.Draw()
     if len(hists_models) > 0:
@@ -248,7 +248,7 @@ def plot_compare(cfg):
         alice_text = get_alice_text(cfg["alice_text"])
         alice_text.Draw("same")
 
-    return canv, hists, hists_syst, hists_models, leg, leg_models, alice_text
+    return canv, hists, graphs_syst, hists_models, leg, leg_models, alice_text
 
 
 def plot_ratio(cfg, hists):
@@ -321,14 +321,14 @@ def main():
     with TFile(os.path.join(cfg["output"]["outdir"],
                f'{cfg["output"]["file"]}.root'), "recreate") as output:
 
-        canv, hists, hists_syst, hists_models, leg, leg_models, alice_text = plot_compare(cfg) # pylint: disable=unused-variable
+        canv, hists, graphs_syst, hists_models, leg, leg_models, alice_text = plot_compare(cfg) # pylint: disable=unused-variable
         output.cd()
         canv.Write()
         save_canvas(canv, cfg, cfg["output"]["file"])
         for _, hist in hists.items():
             hist.Write()
-        for hist in hists_syst:
-            hist.Write()
+        for graph in graphs_syst:
+            graph.Write()
         for hist in hists_models:
             hist.Write()
 
