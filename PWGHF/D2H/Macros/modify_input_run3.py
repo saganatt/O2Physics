0a89af95ac8d4666978c3f773b7d0071e6ded594
saganatt <8majak8@gmail.com>
2024-11-13 16:11:03 +0100

    Scripts to adjust histograms from Run2/3 for crossec compare

---
+++ PWGHF/D2H/Macros/modify_input_run3.py
@@ -0,0 +1,42 @@
+# pylint: disable=missing-function-docstring
+"""
+file:  modify_input.py
+brief: Perform adjustments on input histogram.
+usage: python3 modify_input.py file.root my_histo file_out.root
+author: Maja Karwowska <mkarwowska@cern.ch>, Warsaw University of Technology
+"""
+
+import argparse
+
+from ROOT import (  # pylint: disable=import-error,no-name-in-module
+    gROOT,
+    TFile,
+)
+
+MLHEP_EV_SEL = 20430386.
+NORM = 47092223769.611162532
+
+def main():
+    """
+    Main function.
+    """
+    gROOT.SetBatch(True)
+
+    parser = argparse.ArgumentParser(description="Arguments to pass")
+    parser.add_argument("filename", help="input file with histogram")
+    parser.add_argument("histname", help="histogram name")
+    parser.add_argument("outhistname", help="outhistogram name")
+    parser.add_argument("outname", help="output file for the new histogram")
+    args = parser.parse_args()
+
+    with TFile(args.filename) as fin, TFile(args.outname, "recreate") as fout:
+        hist = fin.Get(args.histname)
+        hist.SetDirectory(0)
+        hist.Scale(MLHEP_EV_SEL / NORM)
+        hist.Scale(1./59400000000) # luminosity scaling
+        fout.cd()
+        hist.Write()
+
+
+if __name__ == "__main__":
+    main()
