02c5e2b5ae113be31bfb26d6635d29d18ec4a016
saganatt <8majak8@gmail.com>
2024-11-29 15:42:29 +0100

    Adjust input switch to histo switch in merge_histos

---
+++ PWGHF/D2H/Macros/merge-yields.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+python merge_histos.py -o /data8/majak/crosssec/merged_yields_approvals.root \
+  -i "/data8/majak/MLHEP/input-fd-23082024/yields-bkg_0.20_0.60_fd_0.00-rebin-1-fixed-sigma.root" \
+  -i "/data8/majak/MLHEP/input-fd-23082024/yields-bkg_0.20_0.60_fd_0.00-rebin-1-fixed-sigma.root" \
+  -i "/data8/majak/MLHEP/input-fd-23082024/yields-bkg_0.20_0.60_fd_0.00-rebin-1-fixed-sigma.root" \
+  -i "/data8/majak/MLHEP/input-fd-23082024/yields-bkg_0.20_0.60_fd_0.00-rebin-1-fixed-sigma.root" \
+  -i "/data8/majak/MLHEP/input-fd-23082024/yields-bkg_0.20_0.60_fd_0.00-rebin-1-fixed-sigma.root" \
+  -i "/data8/majak/MLHEP/input-fd-23082024/yields-bkg_0.20_0.60_fd_0.00-rebin-1-fixed-sigma.root" \
+  -i "/data8/majak/MLHEP/input-fd-23082024/yields-bkg_0.20_0.60_fd_0.00-rebin-1-fixed-sigma.root" \
+  -i "/data8/majak/MLHEP/input-fd-10092024/yields-1224_split_widerange_bkg_0.60_0.60_fd_0.00-fixed-sigma.root" \
+  -i "/data8/majak/MLHEP/input-fd-10092024/yields-1224_split_widerange_bkg_0.60_0.60_fd_0.00-fixed-sigma.root"
