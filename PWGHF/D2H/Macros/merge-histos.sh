488534dc1048cbb2f8220635481ca502f42b4371
saganatt <8majak8@gmail.com>
2024-11-29 15:42:29 +0100

    Adjust input switch to histo switch in merge_histos

--- PWGHF/D2H/Macros/merge-histos.sh
+++ PWGHF/D2H/Macros/merge-histos.sh
@@ -3,11 +3,11 @@
 python merge_histos.py -o /data8/majak/systematics/230824/finalCutVarMerged_0-24.root \
   -n hCorrYieldsPrompt -n hCorrYieldsNonPrompt -n hCorrFracPrompt -n hCorrFracNonPrompt \
   -i /data8/majak/systematics/230824/run3/CutVarLc_pp13TeV_LHC24d3_default.root \
-  /data8/majak/systematics/230824/run3/CutVarLc_pp13TeV_LHC24d3_default.root \
-  /data8/majak/systematics/230824/run3/CutVarLc_pp13TeV_LHC24d3_default.root \
-  /data8/majak/systematics/230824/run3/CutVarLc_pp13TeV_LHC24d3_default.root \
-  /data8/majak/systematics/230824/run3/CutVarLc_pp13TeV_LHC24d3_default.root \
-  /data8/majak/systematics/230824/run3/CutVarLc_pp13TeV_LHC24d3_default.root \
-  /data8/majak/systematics/230824/run3/CutVarLc_pp13TeV_LHC24d3_default.root \
-  /data8/majak/systematics/230824/run3/CutVarLc_pp13TeV_LHC24d3_default_1-24.root \
-  /data8/majak/systematics/230824/run3/CutVarLc_pp13TeV_LHC24d3_default_1-24.root 
+  -i /data8/majak/systematics/230824/run3/CutVarLc_pp13TeV_LHC24d3_default.root \
+  -i /data8/majak/systematics/230824/run3/CutVarLc_pp13TeV_LHC24d3_default.root \
+  -i /data8/majak/systematics/230824/run3/CutVarLc_pp13TeV_LHC24d3_default.root \
+  -i /data8/majak/systematics/230824/run3/CutVarLc_pp13TeV_LHC24d3_default.root \
+  -i /data8/majak/systematics/230824/run3/CutVarLc_pp13TeV_LHC24d3_default.root \
+  -i /data8/majak/systematics/230824/run3/CutVarLc_pp13TeV_LHC24d3_default.root \
+  -i /data8/majak/systematics/230824/run3/CutVarLc_pp13TeV_LHC24d3_default_1-24.root \
+  -i /data8/majak/systematics/230824/run3/CutVarLc_pp13TeV_LHC24d3_default_1-24.root 
