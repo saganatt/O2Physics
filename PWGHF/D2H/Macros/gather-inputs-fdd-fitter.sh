895c3224faf9876fedcd7bd34e56d7f406774091
saganatt <8majak8@gmail.com>
2025-01-31 20:27:48 +0100

    Scripts for fitter fixed sigma

--- PWGHF/D2H/Macros/gather-inputs-fdd-fitter.sh
+++ PWGHF/D2H/Macros/gather-inputs-fdd-fitter.sh
@@ -3,8 +3,8 @@
 MLHEP_DIR="/data8/majak/MLHEP"
 OUTPUT_DIR="${MLHEP_DIR}/input-fd-012025"
 
-RESDIR_PATTERN="/data8/majak/d2h-fitter/012025/results_"
-PERM_PATTERN="fd_"
+RESDIR_PATTERN="/data8/majak/d2h-fitter/0125/results_"
+PERM_PATTERN="fixed-sigma"
 
 for dir in ${RESDIR_PATTERN}*${PERM_PATTERN} ; do
   suffix=${dir##${RESDIR_PATTERN}}
