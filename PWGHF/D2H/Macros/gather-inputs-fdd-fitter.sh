c1c0b9da457dc183dbc6d1a532ee7a09a98d380b
saganatt <8majak8@gmail.com>
2025-02-24 12:35:42 +0100

    Scripts for run with Poly2

--- PWGHF/D2H/Macros/gather-inputs-fdd-fitter.sh
+++ PWGHF/D2H/Macros/gather-inputs-fdd-fitter.sh
@@ -4,9 +4,9 @@ MLHEP_DIR="/data8/majak/MLHEP"
 OUTPUT_DIR="${MLHEP_DIR}/input-fd-012025"
 
 RESDIR_PATTERN="/data8/majak/d2h-fitter/0125/results_"
-PERM_PATTERN="fixed-sigma"
+PERM_PATTERN="poly-fixed-sigma"
 
-for dir in ${RESDIR_PATTERN}*${PERM_PATTERN} ; do
+for dir in ${RESDIR_PATTERN}fd_0.[0-9][0-9][0-9]-${PERM_PATTERN} ; do
   suffix=${dir##${RESDIR_PATTERN}}
   echo $suffix
 
