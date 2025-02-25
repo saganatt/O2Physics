e224272f14ca75d45d199822979605a19c7fcc21
saganatt <8majak8@gmail.com>
2025-02-25 09:30:54 +0100

    Update merge yields

--- PWGHF/D2H/Macros/merge-yields.sh
+++ PWGHF/D2H/Macros/merge-yields.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 # Merge yields at 0.0 for prompt cross section
-python merge_histos.py -o /data8/majak/crosssec/merged_yields_approvals.root \
-  -i "/data8/majak/MLHEP/input-fd-23082024/yields-bkg_0.20_0.60_fd_0.00-rebin-1-fixed-sigma.root" \
-  -i "/data8/majak/MLHEP/input-fd-23082024/yields-bkg_0.20_0.60_fd_0.00-rebin-1-fixed-sigma.root" \
-  -i "/data8/majak/MLHEP/input-fd-23082024/yields-bkg_0.20_0.60_fd_0.00-rebin-1-fixed-sigma.root" \
-  -i "/data8/majak/MLHEP/input-fd-23082024/yields-bkg_0.20_0.60_fd_0.00-rebin-1-fixed-sigma.root" \
-  -i "/data8/majak/MLHEP/input-fd-23082024/yields-bkg_0.20_0.60_fd_0.00-rebin-1-fixed-sigma.root" \
-  -i "/data8/majak/MLHEP/input-fd-23082024/yields-bkg_0.20_0.60_fd_0.00-rebin-1-fixed-sigma.root" \
-  -i "/data8/majak/MLHEP/input-fd-23082024/yields-bkg_0.20_0.60_fd_0.00-rebin-1-fixed-sigma.root" \
-  -i "/data8/majak/MLHEP/input-fd-10092024/yields-1224_split_widerange_bkg_0.60_0.60_fd_0.00-fixed-sigma.root" \
-  -i "/data8/majak/MLHEP/input-fd-10092024/yields-1224_split_widerange_bkg_0.60_0.60_fd_0.00-fixed-sigma.root"
+python merge_histos.py -o "/data8/majak/crosssec/202502/yieldsLcpKpiRun3analysis_fd_0.000.root" \
+  -i "/data8/majak/MLHEP/input-fd-012025/yields-fd_0.000-poly-fixed-sigma.root" \
+  -i "/data8/majak/MLHEP/input-fd-012025/yields-fd_0.000-cheb-fixed-sigma-120-190.root" \
+  -i "/data8/majak/MLHEP/input-fd-012025/yields-fd_0.000-cheb-fixed-sigma-120-190.root" \
+  -i "/data8/majak/MLHEP/input-fd-012025/yields-fd_0.000-cheb-fixed-sigma-120-190.root" \
+  -i "/data8/majak/MLHEP/input-fd-012025/yields-fd_0.000-cheb-fixed-sigma-120-190.root" \
+  -i "/data8/majak/MLHEP/input-fd-012025/yields-fd_0.000-cheb-fixed-sigma-120-190.root" \
+  -i "/data8/majak/MLHEP/input-fd-012025/yields-fd_0.000-cheb-fixed-sigma-120-190.root" \
+  -i "/data8/majak/MLHEP/input-fd-012025/yields-fd_0.000-cheb-fixed-sigma-120-190.root" \
+  -i "/data8/majak/MLHEP/input-fd-012025/yields-fd_0.000-cheb-fixed-sigma-120-190.root" \
+  -i "/data8/majak/MLHEP/input-fd-012025/yields-fd_0.000-cheb-fixed-sigma-120-190.root" \
+  -i "/data8/majak/MLHEP/input-fd-012025/yields-fd_0.000-cheb-fixed-sigma-120-190.root"
