c062bdde0701c5b62e0d8d27d89a3edca0d894cc
saganatt <8majak8@gmail.com>
2025-02-25 15:47:06 +0100

    Fix merge histomass

--- PWGHF/D2H/Macros/merge_histomass.py
+++ PWGHF/D2H/Macros/merge_histomass.py
@@ -18,25 +18,25 @@ def main():
     parser.add_argument("-n", "--histname", action="append", type=str,
                         help="name of histograms to merge")
     parser.add_argument("-o", "--outfile", action="append", type=str, help="Output file")
-    parser.add_argument("-i", "--infile", type=str, nargs="+", help="Input file")
+    parser.add_argument("-i", "--infile", action="append", type=str, help="Input file")
     args = parser.parse_args()
 
     if len(args.outfile) != 1:
         raise ValueError("Provide exactly 1 output file")
 
-    fout = TFile(args.outfile[0], "RECREATE")
+    print(f"infile {args.infile}")
 
-    for name in args.histname:
-        hist_list = []
-        for ind, filename in enumerate(args.infile):
-            fin = TFile(filename)
-            list_hists = [key.GetName() for key in fin.GetListOfKeys() \
-                          if name in key.GetName()]
-            hist = fin.Get(list_hists[ind])
-            fout.cd()
-            hist.Write()
-
-    fout.Close()
+    with TFile(args.outfile[0], "RECREATE") as fout:
+        for name in args.histname:
+            hist_list = []
+            for ind, filename in enumerate(args.infile):
+                fin = TFile(filename)
+                list_hists = [key.GetName() for key in fin.GetListOfKeys() \
+                              if name in key.GetName()]
+                print(f"File {filename} hist list {list_hists} selected {list_hists[ind]}")
+                hist = fin.Get(list_hists[ind])
+                fout.cd()
+                hist.Write()
 
 if __name__ == "__main__":
     main()
