746c74dc24d4b8ca269b273a81f6f99fd6d65db4
saganatt <8majak8@gmail.com>
2025-02-26 21:00:21 +0100

    Update gather script

--- PWGHF/D2H/Macros/gather-inputs-fdd-fitter.sh
+++ PWGHF/D2H/Macros/gather-inputs-fdd-fitter.sh
@@ -4,9 +4,9 @@ MLHEP_DIR="/data8/majak/MLHEP"
 OUTPUT_DIR="${MLHEP_DIR}/input-fd-012025"
 
 RESDIR_PATTERN="/data8/majak/d2h-fitter/0125/results_"
-PERM_PATTERN="-fixed-sigma"
+PERM_PATTERN="-mix-fixed-sigma-240"
 
-for dir in ${RESDIR_PATTERN}*fd_precise*${PERM_PATTERN} ; do
+for dir in ${RESDIR_PATTERN}*${PERM_PATTERN} ; do
   suffix=${dir##${RESDIR_PATTERN}}
   echo $suffix
 
