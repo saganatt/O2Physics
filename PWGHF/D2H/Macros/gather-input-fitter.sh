2214fce33b901227a70b8bafa2b27f3aef5fb86a
saganatt <8majak8@gmail.com>
2025-02-26 20:22:20 +0100

    Update gather and run-fitter to new train data. Use - instead of _ after projections

--- PWGHF/D2H/Macros/gather-input-fitter.sh
+++ PWGHF/D2H/Macros/gather-input-fitter.sh
@@ -3,14 +3,14 @@
 MLHEP_DIR="/data8/majak/MLHEP"
 OUTPUT_DIR="/data8/majak/MLHEP/input-d2h-fitter-012025"
 
-RESDIR_PATTERN="${MLHEP_DIR}/results-24012025-hyp-ml-luigi-cuts_"
-PERM_PATTERN="fd_"
+RESDIR_PATTERN="${MLHEP_DIR}/results-24022025-"
+PERM_PATTERN="newtrain_fd_"
 
-for dir in ${RESDIR_PATTERN}${PERM_PATTERN}* ; do
+for dir in ${RESDIR_PATTERN}${PERM_PATTERN}0.000* ; do
   suffix=${dir##${RESDIR_PATTERN}}
   echo $suffix
   
-  RESPATH="${OUTPUT_DIR}/projections_${suffix}.root"
+  RESPATH="${OUTPUT_DIR}/projections-${suffix}.root"
 
   python gather_input_fitter.py \
     "${dir}/LHC23pp_pass4/Results/resultsdatatot/masshisto.root" \
