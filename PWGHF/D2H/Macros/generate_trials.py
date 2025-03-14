b8402260a5d111a8da22dc7184ae3d399d8ec72f
saganatt <8majak8@gmail.com>
2025-03-14 18:26:21 +0100

    More automated multitrial trials

---
+++ PWGHF/D2H/Macros/generate_trials.py
@@ -0,0 +1,25 @@
+
+
+BASE_TRIALS = (
+    ["min_narrow", "min_narrow2", "min_wide", "min_wide2"],
+    ["max_narrow", "max_narrow2", "max_wide", "max_wide2"],
+    ["fixed_sigma"],
+    ["fixed_mean"],
+    ["double_gauss"],
+    ["poly3"]
+)
+
+
+def generate_trials(trial_classes):
+    combinations = [""]
+    for trial_class in trial_classes:
+        class_comb = []
+        for cur_comb in combinations:
+            for trial in trial_class:
+                class_comb.append(cur_comb + "-" + trial)
+                print(f"{cur_comb}-{trial}")
+        combinations.extend(class_comb)
+    return combinations
+
+
+combinations = generate_trials(BASE_TRIALS)
