b2112804b6b065bce1f585bda861ef9e77d3b14d
saganatt <8majak8@gmail.com>
2025-03-18 15:19:41 +0100

    Merge histos for BDT systematics

--- PWGHF/D2H/Macros/merge-histos.sh
+++ PWGHF/D2H/Macros/merge-histos.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-python merge_histos.py -o /data8/majak/crosssec/202502/fractions/CutVarLc_pp13TeV_LHC23_pass4_wide.root \
+python merge_histos.py -o /data8/majak/systematics/032025/bdt/CutVarLc_pp13TeV_LHC23_pass4_narrow_left.root \
   -n hCorrYieldsPrompt -n hCorrYieldsNonPrompt -n hCorrFracPrompt -n hCorrFracNonPrompt \
   -n hCovPromptPrompt -n hCovPromptNonPrompt -n hCovNonPromptNonPrompt \
-  -i /data8/majak/fdd-results/012025/fdd-results-1-2-wide/CutVarLc_pp13TeV_LHC23_pass4.root \
-  -i /data8/majak/fdd-results/012025/fdd-results-2-3-wide/CutVarLc_pp13TeV_LHC23_pass4.root \
-  -i /data8/majak/fdd-results/012025/fdd-results-3-4-cheb-wide/CutVarLc_pp13TeV_LHC23_pass4.root \
-  -i /data8/majak/fdd-results/012025/fdd-results-4-5-cheb-wide/CutVarLc_pp13TeV_LHC23_pass4.root \
-  -i /data8/majak/fdd-results/012025/fdd-results-5-6-cheb-wide/CutVarLc_pp13TeV_LHC23_pass4.root \
-  -i /data8/majak/fdd-results/012025/fdd-results-4-5-cheb-wide/CutVarLc_pp13TeV_LHC23_pass4.root \
-  -i /data8/majak/fdd-results/012025/fdd-results-4-5-cheb-wide/CutVarLc_pp13TeV_LHC23_pass4.root \
-  -i /data8/majak/fdd-results/012025/fdd-results-4-5-cheb-wide/CutVarLc_pp13TeV_LHC23_pass4.root \
-  -i /data8/majak/fdd-results/012025/fdd-results-10-12-cheb-wide/CutVarLc_pp13TeV_LHC23_pass4.root \
-  -i /data8/majak/fdd-results/012025/fdd-results-10-12-cheb-wide/CutVarLc_pp13TeV_LHC23_pass4.root \
-  -i /data8/majak/fdd-results/012025/fdd-results-16-24-cheb-wide/CutVarLc_pp13TeV_LHC23_pass4.root
+  -i /data8/majak/fdd-results/012025/fdd-results-1-2-narrow-left/CutVarLc_pp13TeV_LHC23_pass4.root \
+  -i /data8/majak/fdd-results/012025/fdd-results-2-3-cheb-narrow-left/CutVarLc_pp13TeV_LHC23_pass4.root \
+  -i /data8/majak/fdd-results/012025/fdd-results-3-4-cheb-narrow-left/CutVarLc_pp13TeV_LHC23_pass4.root \
+  -i /data8/majak/fdd-results/012025/fdd-results-4-5-cheb-narrow-left/CutVarLc_pp13TeV_LHC23_pass4.root \
+  -i /data8/majak/fdd-results/012025/fdd-results-5-6-cheb-narrow-left/CutVarLc_pp13TeV_LHC23_pass4.root \
+  -i /data8/majak/fdd-results/012025/fdd-results-4-5-cheb-narrow-left/CutVarLc_pp13TeV_LHC23_pass4.root \
+  -i /data8/majak/fdd-results/012025/fdd-results-4-5-cheb-narrow-left/CutVarLc_pp13TeV_LHC23_pass4.root \
+  -i /data8/majak/fdd-results/012025/fdd-results-4-5-cheb-narrow-left/CutVarLc_pp13TeV_LHC23_pass4.root \
+  -i /data8/majak/fdd-results/012025/fdd-results-10-12-cheb-narrow-left/CutVarLc_pp13TeV_LHC23_pass4.root \
+  -i /data8/majak/fdd-results/012025/fdd-results-10-12-cheb-narrow-left/CutVarLc_pp13TeV_LHC23_pass4.root \
+  -i /data8/majak/fdd-results/012025/fdd-results-16-24-cheb-narrow-left/CutVarLc_pp13TeV_LHC23_pass4.root
