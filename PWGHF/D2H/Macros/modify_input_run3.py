dfd5eba788d1ca53429e9a3298140920aa9a919a
saganatt <8majak8@gmail.com>
2025-03-25 18:49:09 +0100

    Add rescaling of mult spectra to the modify script

--- PWGHF/D2H/Macros/modify_input_run3.py
+++ PWGHF/D2H/Macros/modify_input_run3.py
@@ -22,6 +22,22 @@ BR = 0.0623
 MLHEP_EV_SEL = 258442910841. # 2 x 10^1
 NORM = 3.0077675e+11
 
+# 2025 values for multiplicity analysis
+EV_SEL_MULT = 290860860000.
+NORM_MB = 249371059919
+NORM_2 = 37884927886
+EV_FACTOR_2 = 0.85
+NORM_3 = 50023302929
+EV_FACTOR_3 = 0.91
+NORM_4 = 49545723906
+EV_FACTOR_4 = 0.96
+NORM_5 = 49300695562
+EV_FACTOR_5 = 0.98
+NORM_6 = 22192632583
+EV_FACTOR_6 = 0.99
+NORM_7 = 2476292886
+EV_FACTOR_7 = 1.0
+
 def main():
     """
     Main function.
@@ -40,9 +56,12 @@ def main():
         hist2 = hist.Clone(args.outhistname)
         hist2.SetDirectory(0)
         #hist2.Scale(0.000001 * MLHEP_EV_SEL / NORM)
-        hist2.Scale(0.000001)
+        #hist2.Scale(0.000001)
         #hist.Scale(1./59400000000) # luminosity scaling, lumi in pb
         #hist.Scale(BR) # BR scaling back
+
+        hist2.Scale(EV_SEL_MULT / NORM_7)
+        hist2.Scale(EV_FACTOR_7)
         fout.cd()
         hist2.Write()
 
