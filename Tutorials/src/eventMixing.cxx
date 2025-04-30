e5360e3464ce48b58fb54570a0ebffa46a1decf2
saganatt <8majak8@gmail.com>
2025-04-30 10:13:47 +0200

    Check mixing with dynamic columns only. Better binning and static assert

--- Tutorials/src/eventMixing.cxx
+++ Tutorials/src/eventMixing.cxx
@@ -46,6 +46,7 @@ struct MixedEvents {
   using BinningType = ColumnBinningPolicy<aod::collision::PosX, aod::collision::PosY>;
   BinningType binningOnPositions{{xBins, yBins}, true};                                            // true is for 'ignore overflows' (true by default)
   SameKindPair<aod::Collisions, aod::Tracks, BinningType> pair{binningOnPositions, 5, -1, &cache}; // indicates that 5 events should be mixed and under/overflow (-1) to be ignored
+
   void process(aod::Collisions const& collisions, aod::Tracks const& tracks)
   {
     LOGF(info, "Input data Collisions %d, Tracks %d ", collisions.size(), tracks.size());
@@ -181,10 +182,11 @@ struct MixedEventsDynamicColumns {
   Preslice<aod::Tracks> perCollison = aod::track::collisionId;
   using AODCollisions = soa::Join<aod::Collisions, aod::EvSels, aod::Mults>;
   std::vector<double> zBins{7, -7, 7};
-  std::vector<double> multBins{VARIABLE_WIDTH, 0, 5, 10, 20, 30, 40, 50, 100.1};
+  std::vector<double> multBins{VARIABLE_WIDTH, 0, 100, 500, 1000, 5000, 10000, 20000};
   using BinningType = ColumnBinningPolicy<aod::collision::PosZ, aod::mult::MultFV0M<aod::mult::MultFV0A, aod::mult::MultFV0C>>;
   BinningType corrBinning{{zBins, multBins}, true};                                       // true is for 'ignore overflows' (true by default)
   SameKindPair<AODCollisions, aod::Tracks, BinningType> pair{corrBinning, 5, -1, &cache}; // indicates that 5 events should be mixed and under/overflow (-1) to be ignored
+  static_assert(is_combinations_generator<SameKindPair<AODCollisions, aod::Tracks, BinningType>>);
 
   void process(AODCollisions const& collisions, aod::Tracks const& tracks)
   {
@@ -501,18 +503,18 @@ struct MixedEventsCounters {
 WorkflowSpec defineDataProcessing(ConfigContext const& cfgc)
 {
   return WorkflowSpec{
-    adaptAnalysisTask<MixedEvents>(cfgc),
-    adaptAnalysisTask<MixedEventsInsideProcess>(cfgc),
-    adaptAnalysisTask<MixedEventsFilteredTracks>(cfgc),
-    adaptAnalysisTask<MixedEventsJoinedCollisions>(cfgc),
-    adaptAnalysisTask<MixedEventsDynamicColumns>(cfgc),
-    adaptAnalysisTask<MixedEventsVariousKinds>(cfgc),
-    adaptAnalysisTask<MixedEventsTriple>(cfgc),
-    adaptAnalysisTask<MixedEventsTripleVariousKinds>(cfgc),
-    adaptAnalysisTask<HashTask>(cfgc),
-    adaptAnalysisTask<MixedEventsWithHashTask>(cfgc),
-    adaptAnalysisTask<MixedEventsPartitionedTracks>(cfgc),
-    adaptAnalysisTask<MixedEventsLambdaBinning>(cfgc),
-    adaptAnalysisTask<MixedEventsCounters>(cfgc),
+    //adaptAnalysisTask<MixedEvents>(cfgc),
+    //adaptAnalysisTask<MixedEventsInsideProcess>(cfgc),
+    //adaptAnalysisTask<MixedEventsFilteredTracks>(cfgc),
+    //adaptAnalysisTask<MixedEventsJoinedCollisions>(cfgc),
+    adaptAnalysisTask<MixedEventsDynamicColumns>(cfgc)//,
+    //adaptAnalysisTask<MixedEventsVariousKinds>(cfgc),
+    //adaptAnalysisTask<MixedEventsTriple>(cfgc),
+    //adaptAnalysisTask<MixedEventsTripleVariousKinds>(cfgc),
+    //adaptAnalysisTask<HashTask>(cfgc),
+    //adaptAnalysisTask<MixedEventsWithHashTask>(cfgc),
+    //adaptAnalysisTask<MixedEventsPartitionedTracks>(cfgc),
+    //adaptAnalysisTask<MixedEventsLambdaBinning>(cfgc),
+    //adaptAnalysisTask<MixedEventsCounters>(cfgc),
   };
 }
